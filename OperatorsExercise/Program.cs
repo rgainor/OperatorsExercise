48456d730399fd0f3e5a9ba9d1a20fceb29d3f8f
Ryan Gainor <rmgainor@gmail.com>
2023-05-15 10:49:08 -0500

    Operators Exercise Completed

---
+++ OperatorsExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace OperatorsExercise;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ExerciseMethod();
+        AreaOfCircleMethod();
+    }
+
+    static void ExerciseMethod()
+    {
+        int a = 17;
+        int b = 4;
+        int quotient = a / b;
+        int remainder = a % b;
+        Console.WriteLine($"{a}/{b} = {quotient} remainder {remainder}");
+    }
+    static double AreaOfCircleMethod()
+    {
+        Console.WriteLine("What is the radius of your circle?");
+        var radius = double.Parse(Console.ReadLine());
+
+        double area = Math.PI * radius *radius;
+        Console.WriteLine($"The area of a circle with a radius of {radius} is {area}");
+        return area;
+
+    }
+}
